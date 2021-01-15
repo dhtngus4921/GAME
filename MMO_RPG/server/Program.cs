40bc058a48113464a7ec5f3cad9f2171eb24f712
dhtngus4921 <44641501+dhtngus4921@users.noreply.github.com>
2021-01-15 16:35:38 +0900

    Add files via upload

--- MMO_RPG/server/Program.cs
+++ MMO_RPG/server/Program.cs
@@ -4,30 +4,10 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 
-namespace ServerCore
+namespace DummyClient
 {
     class Program
     {
-        static Listener _listener = new Listener();
-        static void OnAcceptHandler(Socket clientSocket)
-        {
-            try
-            {
-                Session session = new Session();
-                session.Start(clientSocket);
-
-                byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to Server! ");
-
-                session.Send(sendBuff);
-
-                Thread.Sleep(10000);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-        }
-
         static void Main(string[] args)
         {
             string host = Dns.GetHostName();
@@ -35,13 +15,36 @@ namespace ServerCore
             IPAddress ipAddr = ipHost.AddressList[0];
             IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-            _listener.Init(endPoint, OnAcceptHandler);
-            Console.WriteLine("Listening...");
-
             while (true)
             {
-                ;
-            }        
+                Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+                    socket.Connect(endPoint);
+                    Console.WriteLine($"Connected To {socket.RemoteEndPoint.ToString()}");
+
+                    for (int i = 0; i < 5; i++)
+                    {
+                        byte[] sendBuff = Encoding.UTF8.GetBytes($"hello server {i}");
+                        int sendBytes = socket.Send(sendBuff);
+                    }
+
+                    byte[] recvbuff = new byte[1024];
+                    int recvbytes = socket.Receive(recvbuff);
+                    string recvData = Encoding.UTF8.GetString(recvbuff, 0, recvbytes);
+                    Console.WriteLine($"[from server] {recvData}");
+
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+
+                Thread.Sleep(100);
+            }
         }
     }
 }
