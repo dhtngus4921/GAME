f65e6a1574ce83eab45363b2402759d18dc3dd13
dhtngus4921 <44641501+dhtngus4921@users.noreply.github.com>
2021-01-29 16:59:18 +0900

    Add files via upload

--- MMO_RPG/server/DummyClient/ClientProgram.cs
+++ MMO_RPG/server/DummyClient/ClientProgram.cs
@@ -17,20 +17,23 @@ namespace DummyClient
             IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
             Connector connector = new Connector();
-            connector.Connect(endPoint, () => { return SessionManager.Instance.Generate(); });
+
+            connector.Connect(endPoint, 
+                () => { return SessionManager.Instance.Generate(); },
+                10);
 
             while (true)
             {
                 try
                 {
-
+                    SessionManager.Instance.SendForEach();
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine(e.ToString());
                 }
 
-                Thread.Sleep(100);
+                Thread.Sleep(250);
             }
         }
     }
--- MMO_RPG/server/DummyClient/SessionManager.cs
+++ MMO_RPG/server/DummyClient/SessionManager.cs
@@ -12,6 +12,20 @@ namespace DummyClient
         List<ServerSession> _sessions = new List<ServerSession>();
         object _lock = new object();
 
+        public void SendForEach()
+        {
+            lock (_lock)
+            {
+                foreach(ServerSession session in _sessions)
+                {
+                    C_Chat chatPacket = new C_Chat();
+                    chatPacket.chat = $"Hello Server!";
+                    ArraySegment<byte> segment = chatPacket.Write();
+
+                    session.Send(segment);
+                }
+            }
+        }
         public ServerSession Generate()
         {
             lock (_lock)
