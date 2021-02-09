e6d98d97ee42375a553a110f4fa6bf717865d9ef
dhtngus4921 <44641501+dhtngus4921@users.noreply.github.com>
2021-02-09 15:57:20 +0900

    Add files via upload

--- MMO_RPG/server/Server/ServerProgram.cs
+++ MMO_RPG/server/Server/ServerProgram.cs
@@ -4,44 +4,40 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using ServerCore;
 
-/* sendBuff의 경우 내부(session)에서 복사해서 사용하는 것보다 외부에서 선언하고 끌어가서 사용하는 것이 더 효율적임
-*  예)
-*   byte[] sendBuff = new byte[1024];
-*   for( ;;)
-*     Session.Send();
-*/
 namespace Server
 {
-    class ServerProgram
-    {
-        static Listener _listener = new Listener();
-        public static GameRoom Room = new GameRoom();
+	class Program
+	{
+		static Listener _listener = new Listener();
+		public static GameRoom Room = new GameRoom();
 
-        static void FlushRoom()
-        {
-            Room.Push(() => Room.Flush());
-            JobTimer.Instance.Push(FlushRoom, 250);
-        }
-        //달라진점. 어떤 세션을 만들것인지만 정의해주고 나머지는 내부에서 처리, 프로그램 보안성 향상   
-        static void Main(string[] args)
-        { 
-            string host = Dns.GetHostName();
-            IPHostEntry ipHost = Dns.GetHostEntry(host);
-            IPAddress ipAddr = ipHost.AddressList[0];
-            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+		static void FlushRoom()
+		{
+			Room.Push(() => Room.Flush());
+			JobTimer.Instance.Push(FlushRoom, 250);
+		}
 
-            //gameSession을 만들기로 정의
-            _listener.Init(endPoint, () => { return SessionManager.Instance.Generate(); });
-            Console.WriteLine("Listening...");
+		static void Main(string[] args)
+		{
+			// DNS (Domain Name System)
+			string host = Dns.GetHostName();
+			IPHostEntry ipHost = Dns.GetHostEntry(host);
+			IPAddress ipAddr = ipHost.AddressList[0];
+			IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-            JobTimer.Instance.Push(FlushRoom);
+			_listener.Init(endPoint, () => { return SessionManager.Instance.Generate(); });
+			Console.WriteLine("Listening...");
 
-            while (true)
-            {
-                JobTimer.Instance.Flush();
-            }
-        }
-    }
+			//FlushRoom();
+			JobTimer.Instance.Push(FlushRoom);
+
+			while (true)
+			{
+				JobTimer.Instance.Flush();
+			}
+		}
+	}
 }
